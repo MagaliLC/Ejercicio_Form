2ebd0862c8eb009d2ae20d2b4af73616d914c110
Magali <magalilescanocorrea@gmail.com>
2019-06-07 13:45:05 +0200

    Alert creado

--- app/src/main/java/com/example/form_exercise/MainActivity.java
+++ app/src/main/java/com/example/form_exercise/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.form_exercise;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,13 +25,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void deleteForm(View view) {
-        etName.setText("");
-        etEmail.setText("");
-        etTel.setText("");
-        etPass.setText("");
-    }
-
     public void checkForm(View view) {
         String n = etName.getText().toString();
        /* if ((etName.getText().toString().trim().equalsIgnoreCase("")) || (etEmail.getText().toString().trim().equalsIgnoreCase("")) || (etPass.getText().toString().trim().equalsIgnoreCase("")) || (etTel.getText().toString().trim().equalsIgnoreCase(""))){
@@ -50,5 +45,41 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, getString(R.string.ok), Toast.LENGTH_LONG).show();
         }
+    }
+
+    public void openDialog(View view) {
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+
+                this);
+
+        alertDialogBuilder.setTitle(getString(R.string.app_name));
+
+        alertDialogBuilder.setMessage(getString(R.string.deleteAll))
+
+                .setCancelable(false)
+
+                .setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        etName.setText("");
+                        etEmail.setText("");
+                        etTel.setText("");
+                        etPass.setText("");
+                    }
+                })
+
+                .setNegativeButton(getString(R.string.no), new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        alertDialog.show();
     }
 }
